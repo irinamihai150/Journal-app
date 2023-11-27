38d8bdde86381113c39921ab792e34b85f5dd005
irinamihai150 <irinamihai150@gmail.com>
2023-11-27 15:08:15 +0000

    header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { Navbar, Nav, Container } from "react-bootstrap"
+import { FaUser } from "react-icons/fa"
+
+const Header = () => {
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+				<Container>
+					<Navbar.Brand href='/'>
+						Journal
+						<Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+						<Navbar.Collapse id='basic-navbar-nav'> </Navbar.Collapse>
+						<Nav className='ms-auto'>
+							<Nav.Link href='/login'>
+								<FaUser />
+								Sign In
+							</Nav.Link>
+						</Nav>
+					</Navbar.Brand>
+				</Container>
+			</Navbar>
+		</header>
+	)
+}
+
+export default Header
